b2c72b41f1f09da795c48f3350a9b5c3309f2fa4
Mihaylov <misho.mihaylov@gmail.com>
2024-01-09 23:44:46 +0200

    Initial commit

---
+++ src/StacksAndQueues/Lab/BrowserHistory_01.java
@@ -0,0 +1,14 @@
+package StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class BrowserHistory_01 {
+    public static void main(String[] args) {
+
+        Scanner kbInput = new Scanner(System.in);
+
+
+
+
+    }
+}
