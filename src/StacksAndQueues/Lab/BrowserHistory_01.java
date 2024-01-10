ed0d82b6b74cbf258deb320def715051e6f6cdd4
Mihaylov <misho.mihaylov@gmail.com>
2024-01-10 23:00:26 +0200

    Update!

--- src/StacksAndQueues/Lab/BrowserHistory_01.java
+++ src/StacksAndQueues/Lab/BrowserHistory_01.java
@@ -1,11 +1,42 @@
 package StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BrowserHistory_01 {
     public static void main(String[] args) {
 
         Scanner kbInput = new Scanner(System.in);
+
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+
+        String input = kbInput.nextLine();
+
+        while (!"Home".equalsIgnoreCase(input)){
+
+            if (!"Back".equalsIgnoreCase(input)){
+
+                stack.push(input);
+
+                System.out.println(input);
+            }else {
+                if (stack.size() <=1){
+                    System.out.println("no previous URLs");
+                }else {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                }
+            }
+
+
+
+
+            input = kbInput.nextLine();
+
+
+        }
 
 
 
