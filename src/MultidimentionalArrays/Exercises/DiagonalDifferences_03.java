d52cfbeace4cd8cf9a753b01eefcae8ee0d38ab3
Mihail Mihaylov <misho.mihaylov@gmail.com>
2024-01-18 17:45:19 +0200

    Update!

--- src/MultidimentionalArrays/Exercises/DiagonalDifferences_03.java
+++ src/MultidimentionalArrays/Exercises/DiagonalDifferences_03.java
@@ -11,6 +11,29 @@ public class DiagonalDifferences_03 {
 
         int [][] matrix = new int[matrixSize][matrixSize];
 
+        int pDiagSum = 0;
+        int sDiagSum = 0;
+
+        fillMatrix(matrix, kbInput);
+
+        for (int row = 0; row < matrix.length; row++) {
+
+            for (int col = 0; col < matrix[row].length ; col++) {
+
+                if (row == col){
+                    pDiagSum += matrix[row][col] ;
+                }
+                if (col == (matrix[row].length - 1) - row ){
+                    sDiagSum += matrix[row][col];
+                }
+            }
+
+        }
+
+        System.out.println(Math.abs(pDiagSum - sDiagSum));
+    }
+
+    private static void fillMatrix(int[][] matrix, Scanner kbInput) {
         for (int i = 0; i < matrix.length; i++) {
 
             matrix[i] = Arrays.stream(kbInput.nextLine().split("\\s+"))
