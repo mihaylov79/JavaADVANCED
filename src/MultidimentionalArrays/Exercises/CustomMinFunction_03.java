9e16f770c5814514457493c80e31dc0946709c46
Mihaylov <misho.mihaylov@gmail.com>
2024-01-27 22:41:53 +0200

    Update!

---
+++ src/MultidimentionalArrays/Exercises/CustomMinFunction_03.java
@@ -0,0 +1,13 @@
+package MultidimentionalArrays.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CustomMinFunction_03 {
+    public static void main(String[] args) {
+        Scanner kbInput = new Scanner(System.in);
+
+        System.out.println(Arrays.stream(kbInput.nextLine().split("\\s+"))
+                    .map(Integer::parseInt).min(Integer::compare).get());
+    }
+}
